55c445ebb6922c74d51461d39da89f54667b78d6
carlos morais <cadu_morais26@hotmail.com>
2024-02-13 20:43:03 -0300

    correção na logica da criação randomica da conexão que ficava igual a partida  e da chegada que ficava igual a conexão

--- js/generete_event.js
+++ js/generete_event.js
@@ -1,35 +1,49 @@
-//creation of a radom outbound 
+// Generate a random number between 1 and 15 excluding the selected departure
+function getRandomValue(exclude) {
+  let randomValue;
+  do {
+      randomValue = Math.floor(Math.random() * 15) + 1;
+  } while (randomValue === exclude);
+  return randomValue;
+}
+
 document.getElementById('Outbound-button').addEventListener('click', function() {
-    // Get the select element
-    var selectElement = document.getElementById('Outbound');
-  
-    // Generate a random number between 1 and 15
-    var randomValue = Math.floor(Math.random() * 15) + 1;
-  
-    // Set the selected option based on the random value
-    selectElement.value = randomValue;
-  
-    // If you want to trigger the change event manually, uncomment the following line
-     selectElement.dispatchEvent(new Event('change'));
-  });
-// end of the creation of a radom outbound 
+  // Get the select element
+  var selectElement = document.getElementById('Outbound');
 
-//creation of a radom arrival
-    document.getElementById('Arrival-button').addEventListener('click', function() {
-        // Get the select element
-        var selectElement = document.getElementById('Arrival');
-      
-        // Generate a random number between 1 and 15
-        var randomValue = Math.floor(Math.random() * 15) + 1;
-      
-        // Set the selected option based on the random value
-        selectElement.value = randomValue;
-      
-        // If you want to trigger the change event manually, uncomment the following line
-         selectElement.dispatchEvent(new Event('change'));
-      });
-// end of the creation of a radom arrival 
+  // Get the selected departure
+  var departureValue = parseInt(document.getElementById('Departure').value);
 
+  // Generate a random number excluding the selected departure
+  var randomValue = getRandomValue(departureValue);
+
+  // Set the selected option based on the random value
+  selectElement.value = randomValue;
+
+  // If you want to trigger the change event manually, uncomment the following line
+  selectElement.dispatchEvent(new Event('change'));
+});
+
+document.getElementById('Arrival-button').addEventListener('click', function() {
+  // Get the select element
+  var selectElement = document.getElementById('Arrival');
+
+  // Get the selected departure and outbound
+  
+  var outboundValue = parseInt(document.getElementById('Outbound').value);
+
+  // Generate a random number excluding the selected departure and outbound
+  var randomValue;
+  do {
+      randomValue = Math.floor(Math.random() * 15) + 1;
+  } while (randomValue === outboundValue);
+
+  // Set the selected option based on the random value
+  selectElement.value = randomValue;
+
+  // If you want to trigger the change event manually, uncomment the following line
+  selectElement.dispatchEvent(new Event('change'));
+});
 
 
 
--- teste.js
+++ teste.js
@@ -1,35 +1,46 @@
-document.getElementById('Arrival-passengers-button').addEventListener('click', function() {
-      
-    // Generate random weight for passengers within cargo hold limit of 692lbs
-const maxPassengerWeight = 250; // Max weight per passenger
-let totalPassengerWeight = 0;
-
-// Generate passenger weights until total passenger weight is within cargo hold limit
-for (let i = 0; i < numPassengers; i++) {
-  const passengerWeight = Math.random() * maxPassengerWeight;
-  if (totalPassengerWeight + passengerWeight <= z) {
-    totalPassengerWeight += passengerWeight;
-  } else {
-    break; // Stop adding passengers if cargo hold limit is reached
-  }
+// Generate a random number between 1 and 15 excluding the selected departure
+function getRandomValue(exclude) {
+  let randomValue;
+  do {
+      randomValue = Math.floor(Math.random() * 15) + 1;
+  } while (randomValue === exclude);
+  return randomValue;
 }
 
-// Update input field with generated passengers and their weight
-document.getElementById('Arrival-passengers').value = `${i} passengers, Total Weight: ${totalPassengerWeight.toFixed(2)} lbs`;
-  });
-  
-  document.getElementById('Outbound-cargo-button').addEventListener('click', function() {
-    // Aircraft values
-    const x = 6702; // Aircraft with fuel + pilot weight
-    const y = 7394; // Max take-off weight
-  
-    // Get total weight of passengers
-    const passengersWeight = parseFloat(document.getElementById('Arrival-passengers').value.split(':')[1].split('lbs')[0]);
-  
-    // Calculate outbound cargo weight
-    const outboundCargoWeight = y - (x + passengersWeight);
-  
-    // Update input field with generated cargo weight
-    document.getElementById('Outbound-cargo').value = `${outboundCargoWeight.toFixed(2)} lbs`;
-  });
-  
+document.getElementById('Outbound-button').addEventListener('click', function() {
+  // Get the select element
+  var selectElement = document.getElementById('Outbound');
+
+  // Get the selected departure
+  var departureValue = parseInt(document.getElementById('Departure').value);
+
+  // Generate a random number excluding the selected departure
+  var randomValue = getRandomValue(departureValue);
+
+  // Set the selected option based on the random value
+  selectElement.value = randomValue;
+
+  // If you want to trigger the change event manually, uncomment the following line
+  selectElement.dispatchEvent(new Event('change'));
+});
+
+document.getElementById('Arrival-button').addEventListener('click', function() {
+  // Get the select element
+  var selectElement = document.getElementById('Arrival');
+
+  // Get the selected departure and outbound
+  var departureValue = parseInt(document.getElementById('Departure').value);
+  var outboundValue = parseInt(document.getElementById('Outbound').value);
+
+  // Generate a random number excluding the selected departure and outbound
+  var randomValue;
+  do {
+      randomValue = Math.floor(Math.random() * 15) + 1;
+  } while (randomValue === departureValue || randomValue === outboundValue);
+
+  // Set the selected option based on the random value
+  selectElement.value = randomValue;
+
+  // If you want to trigger the change event manually, uncomment the following line
+  selectElement.dispatchEvent(new Event('change'));
+});
